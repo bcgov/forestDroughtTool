e836b8f4fc60b5727444feba640b868acb4a0427
Griesbauer <Hardy.Griesbauer@gov.bc.ca>
2019-09-24 10:46:43 -0700

    asd

---
+++ R/asmrClass.R
@@ -0,0 +1,12 @@
+#' ASMR category data for AET/PET ratios.
+#'
+#' Use this dataset when classifying AET/PET ratios into ASMR categories.
+#'
+#' @format A data frame with 6 rows and 2 variables
+#' \describe{
+#' \item{asmrUL}{asmrUL: Upper limit of AET/PET ratio for that category}
+#' \item{class}{class: ASMR class}
+#'   ...
+#' }
+
+"asmrClass"
