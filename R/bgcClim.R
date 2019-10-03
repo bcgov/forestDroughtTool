3d6c9daff5f3130b046e0f9cc35973a648af996f
Griesbauer <Hardy.Griesbauer@gov.bc.ca>
2019-10-03 13:42:34 -0700

    asd

---
+++ R/bgcClim.R
@@ -0,0 +1,15 @@
+#' Current and future climate data for BGC units in British Columbia
+#'
+#' A dataset containing normal and future climate data.
+#' Data are from ClimateBC (v?)
+#'
+#' @format A data frame with 1470 rows and 250 variables
+#' \describe{
+#' \item{BGC: biogeoclimatic unit}
+#' \item{period: climatic period}
+#' \item{assorted climate variables}
+#' * ...
+#' }
+
+"bgcCLim"
+#> [1] "bgcClim"
